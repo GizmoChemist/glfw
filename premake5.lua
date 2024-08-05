6a20db63b442a94ea0ebff5dea11b1fefef2988c
GizmoChemist <103856608+GizmoChemist@users.noreply.github.com>
2024-08-04 19:09:01 -0500

    Update premake5.lua
    Trying to fix some linker errors.

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,10 @@ project "GLFW"
 
 	files
 	{
+		-- Temporary Test
+		"src/**.h",
+		"src/**.c",
+	
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
@@ -22,24 +26,24 @@ project "GLFW"
 		systemversion "latest"
 		staticruntime "On"
 
-	files
-	{
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_monitor.c",
-		"src/win32_time.c",
-		"src/win32_thread.c",
-		"src/win32_window.c",
-		"src/wgl_context.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c"
-	}
-	
-	defines
-	{
-		"_GLFW_WIN32",
-		"_CRT_SECURE_NO_WARNINGS"
-	}
+		files
+		{
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+		
+		defines
+		{
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
 
 filter { "system:windows", "configurations:Release" }
-	buildoptions "/MT"
+	--buildoptions "/MT"
